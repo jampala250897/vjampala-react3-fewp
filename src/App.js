7a1cc4e27bcc51aee442ca44122a29625214471d
VINEETH KUMAR JAMPAL <jampala250897@gmail.com>
2024-11-18 16:58:35 -0600

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TaskManager from "./components/TaskManager";
+import UserContext from "./context/UserContext";
 
-function App() {
+const App = () => {
+  const user = { name: "Vineeth Kumar Jampal", id: 1 }; // Simulated user data
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={user}>
+      <TaskManager />
+    </UserContext.Provider>
   );
-}
+};
 
 export default App;
